b16a73d0ebf74bd4dad82965957393f533be115f
Sanja Radulovic <radulovicsanja99@gmail.com>
2026-02-12 10:43:14 +0100

    Kreiranje repozitorijuma

---
+++ src/main/java/com/example/budgetFlow/BudgetFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budgetFlow/BudgetFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
